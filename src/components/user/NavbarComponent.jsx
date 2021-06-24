887041e35635e7fc30bf6d9ac6388ae44839713c
Atef <mahmoudbenatef@gmail.com>
2021-06-24 21:46:11 +0200

    inhance admin style

--- src/components/user/NavbarComponent.jsx
+++ src/components/user/NavbarComponent.jsx
@@ -2,6 +2,8 @@ import { useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { authContext } from "../../contexts/authContext";
 import { mySessionStorage } from "../../helper/LocalStorge";
+import  "../../styles/Nav.css"
+import { BASE_URL } from "../../API/urls";
 export default function NavbarComponent() {
   const authentication = useContext(authContext);
   const history = useHistory();
@@ -16,31 +18,102 @@ export default function NavbarComponent() {
   };
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light justify-content-between ">
+      <nav className="navbar navbar-expand-lg navbar-light justify-content-between " 
+      // style={{backgroundColor:"#212529", color:"white"}}
+      >
         <div >
           {
             isAdmin() ?(
+              <ul className="navbar-nav mr-auto">
+              <li >
+  
                     <Link
                         className="nav-link"
                         to={isAdmin() ? "/admin/categories" : "/categories"}
                     >
                       Categories
-                    </Link>              )
+                    </Link>  
+                    </li>
+                    <li >
+  
+                    <Link
+                        className="nav-link"
+                        to={ "/admin/camps"}
+                    >
+                      Camps
+                    </Link>  
+                    </li>
+                    <li >
+  
+                    <Link
+                        className="nav-link"
+                        to={ "/admin/meals"}
+                    >
+                      meals
+                    </Link>  
+                    </li>
+                    
+                    
+                    </ul>
+                    
+                    )
                 :(
                     <>
-                      <Link
+                      <ul className="navbar-nav mr-auto">
+            <li >
+            <Link
                           className="nav-link"
                           to={"/user/questioner" }
                       >
                         Questioner
                       </Link>
-
+                    
+                    
+              </li>
+            <li >
+            <Link
+                          className="nav-link"
+                          to={"/user/camps"}
+                      >
+                        Camps
+                      </Link>
+            </li>
+            <li>
+            <Link
+                          className="nav-link"
+                          to={"/user/plan"}
+                      >
+                        Plan
+                      </Link>
+            </li>
+            </ul>
+                     
                     </>
                 )
           }
         </div>
         <div className={" "}>
-         <button className="nav-link" onClick={logout}> Logout</button>
+        <ul className="navbar-nav mr-auto">
+            <li >
+           
+        <img
+                style={{ borderRadius: "2rem" }}
+                src={BASE_URL + "/" + mySessionStorage.getCurrentUser().avatar}
+                height="40"
+                width="40"
+                alt=""
+              />
+        </li>
+
+            <li className=" nav-link">
+              {mySessionStorage.getCurrentUser().firstname}{" "}
+              {mySessionStorage.getCurrentUser().lastname}
+            </li>
+
+            <li>
+         <li className="nav-link" onClick={logout}> Logout</li>
+         </li>
+         </ul>
         </div>
       </nav>
 
