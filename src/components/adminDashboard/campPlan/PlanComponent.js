b6a0af2116ac7a8a710849a8976b9a400e7a9b83
Atef <mahmoudbenatef@gmail.com>
2021-06-25 15:47:16 +0200

    modify days style

--- src/components/adminDashboard/campPlan/PlanComponent.js
+++ src/components/adminDashboard/campPlan/PlanComponent.js
@@ -1,6 +1,8 @@
 import {
     useParams
 } from "react-router-dom";
+import { Table } from "react-bootstrap";
+
 import { useEffect, useState } from "react";
 import { ApiServices } from "../../../API/ApiServices"
 import { Link } from "react-router-dom";
@@ -20,25 +22,43 @@ export default function PlanComponent() {
 
     return (
         <>
-            <div className={"container"}>
-                <h1>Camp days</h1>
+            <div>
+                <div className="row justify-content-center mt-4 ">
+                    <div className="col-md-8 ">
+                        <Table striped bordered hover variant="dark" className={"mt-5"}>
+                            <thead key={-1}>
+                                <tr style={{ textAlign: "center", fontSize: "40px" }}>
+                                    <th scope="col">Camp Days</th>
 
-                {
-                    camp &&
-                    [0, 1, 2, 3, 4, 5, 6].map((numberOfDaysToAdd) => {
-                        var someDate = new Date(camp.date);
-                        someDate.setDate(someDate.getDate() + numberOfDaysToAdd);
-                        return (
+                                </tr>
+                            </thead>
+                            <tbody>
 
-                            <Link
-                                className="nav-link"
-                                to={`/admin/camp/${id}/plan/${someDate}`}
-                            >
-                                {someDate.toLocaleDateString()}
-                            </Link>
-                        )
-                    })
-                }
+
+                                {
+                                    camp &&
+                                    [0, 1, 2, 3, 4, 5, 6].map((numberOfDaysToAdd) => {
+                                        var someDate = new Date(camp.date);
+                                        someDate.setDate(someDate.getDate() + numberOfDaysToAdd);
+                                        return (
+                                            <tr key={numberOfDaysToAdd}>
+                                                <td style={{ textAlign: "center" }}>
+
+                                                    <Link style={{ color: "white" }}
+                                                        className="nav-link"
+                                                        to={`/admin/camp/${id}/plan/${someDate}`}
+                                                    >
+                                                        {someDate.toLocaleDateString()}
+                                                    </Link>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </Table>
+                    </div>
+                </div>
             </div>
         </>
     )
