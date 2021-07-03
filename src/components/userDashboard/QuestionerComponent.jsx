57ce867e649655c4aee1788509c427137a7b323d
Atef <mahmoudbenatef@gmail.com>
2021-07-03 14:56:32 +0200

    set user can't see camps till fill questioner

--- src/components/userDashboard/QuestionerComponent.jsx
+++ src/components/userDashboard/QuestionerComponent.jsx
@@ -49,7 +49,8 @@ export default function QuesionerComponent() {
     
             })
             :
-        ApiServices.saveQuestioner(questioner).then(() => {
+        ApiServices.saveQuestioner(questioner).then((data) => {
+            mySessionStorage.setCurrentUser(data.data);
             history.push("/user");
 
         }).catch(() => {
